684e12300a451b50c7bbffb06795e79f250094ce
jaeyounging <5061yo@naver.com>
2024-07-24 22:56:37 +0900

    ✅ meals page

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,22 @@
+import Link from "next/link";
+import styles from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+
 export default function MealsPage() {
   return (
-    <main>
-      <p>meals page🍕🍔🍟🍜🍦</p>
-    </main>
+    <>
+      <header className={styles.header}>
+        <h1>
+          Delicious meals, created <span className={styles.highlight}>by you</span>
+        </h1>
+        <p>Choose your favorite recipe and cook it yourself. It is easy and fun!</p>
+        <p className={styles.cta}>
+          <Link href={"/meals/share"}>Share your favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={styles.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 }
---
+++ components/meals/meals-grid.js
@@ -0,0 +1,14 @@
+// 그리드에 여러가지 음식을 추가
+import MealItem from "./meal-item";
+import styles from "./meals-grid.module.css";
+
+export default function MealsGrid({ meals }) {
+  return;
+  <ul className={styles.meals}>
+    {meals.map((meal) => (
+      <li key={meal.id}>
+        <MealItem {...meal} />
+      </li>
+    ))}
+  </ul>;
+}
