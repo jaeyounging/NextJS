78027cdafc2ae1348a6272ca9678e66b646a4d78
jaeyounging <5061yo@naver.com>
2024-07-24 21:50:05 +0900

    ✅ main-header

---
+++ components/main-header/nav-link.js
@@ -0,0 +1,18 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import styles from "./nav-link.module.css";
+
+export default function NavLink({ href, children }) {
+  const path = usePathname(); // NextJS 훅을 이용한 활동 경로 파악 - 도메인 다음 부분에 경로를 제공
+
+  return (
+    <Link
+      href={href}
+      className={path.startsWith(href) ? `${styles.link} ${styles.active}` : styles.link}
+    >
+      {children}
+    </Link>
+  );
+}
