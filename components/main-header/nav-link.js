f993a3a6d8d3f1fe9e85b7e45f7c2e685def91f1
jaeyounging <5061yo@naver.com>
2024-07-25 14:08:45 +0900

    📝 주석 수정

--- components/main-header/nav-link.js
+++ components/main-header/nav-link.js
@@ -5,7 +5,7 @@ import { usePathname } from "next/navigation";
 import styles from "./nav-link.module.css";
 
 export default function NavLink({ href, children }) {
-  const path = usePathname(); // NextJS 훅을 이용한 활동 경로 파악 - 도메인 다음 부분에 경로를 제공
+  const path = usePathname(); // NextJS 훅을 이용한 활동 경로 파악 - 도메인 다음 부분에 경로를 반환
 
   return (
     <Link
