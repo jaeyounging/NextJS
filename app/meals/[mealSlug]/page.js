230d75b855cdf639346255d13e17881b5737c5c7
jaeyounging <5061yo@naver.com>
2024-07-25 15:50:11 +0900

    ✅ meal detail page

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,4 +1,33 @@
 // localhost:3000/meals/something
-export default function MealDetailsPage() {
-  return <h1>Meal Details</h1>;
+import Image from "next/image";
+
+import { getMeal } from "@/lib/meals";
+import styles from "./page.module.css";
+
+export default function MealDetailsPage({ params }) {
+  const meal = getMeal(params.mealSlug);
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={styles.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={styles.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={styles.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={styles.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
+    </>
+  );
 }
